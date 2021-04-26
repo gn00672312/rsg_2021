224fd403767b3709305b834e77621aa0de03a053
roywu <gn02729362@hotmail.com>
2021-04-26 23:46:50 +0800

    add rsg_logo and test banner.

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,13 +1,7 @@
 <template>
   <el-container id="main">
     <el-header id="header">
-      <el-row>
-        <el-col :span="24">
-          <div id="banner">
-          </div>
-        </el-col>
-      </el-row>
-      <el-menu :default-active="activeIndex" class="el-menu" mode="horizontal">
+      <el-menu :default-active="activeIndex" class="el-menu rsg-theme" mode="horizontal">
         <el-menu-item class="dock-right" index="6">票務</el-menu-item>
         <el-menu-item class="dock-right" index="5">贊助夥伴</el-menu-item>
         <el-menu-item class="dock-right" index="4">Scrum Alliance</el-menu-item>
@@ -17,18 +11,20 @@
       </el-menu>
     </el-header>
     <el-main>
+      <el-row style="position: relative;">
+        <img class="banner-bg" src="@/assets/imgs/test-taipei.jpg"/>
+        <div class="banner-content">
+          <div><font-awesome-icon :icon="['far', 'calendar-alt']"/> 11-13 Nov, 2021</div>
+          <img src="@/assets/imgs/RSG_logo_2021_Taipei.png"/>
+        </div>
+      </el-row>
       系統建置中...
-    </el-main>
-    <el-footer id="footer">
-      <el-row>
-        <el-col :span="12">
-          關於ACT台灣敏捷協會
-        </el-col>
-        <el-col :span="12">
-          關於TAT台灣敏捷部落
+      <el-row id="footer">
+        <el-col :span="8" :offset="8" style="text-align:center">
+          <img id='footer-logo' src="@/assets/imgs/RSG_logo_2021_Taipei.png"/>
         </el-col>
       </el-row>
-    </el-footer>
+    </el-main>
     <el-backtop target=".page-component__scroll .el-scrollbar__wrap"></el-backtop>
   </el-container>
 </template>
@@ -57,22 +53,28 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/scss/var.scss";
 
+.el-header, .el-main, .el-container {
+  padding: 0 !important;
+}
 #main {
   height: 100%;
 }
 
-.el-header, .el-footer{
-  padding: 0;
+.banner-bg {
+  opacity: 0.6;
+  width: 100%;
+  height: auto;
 }
 
-#banner {
-  height: 100%;
-  width: 100%;
-  background-image: url('~@/assets/imgs/taipei.jpg');
+.banner-content {
+  position: absolute;
+  top: 50%;
+  left: 25%;
+  transform: translate(-50%, -50%);
 }
 
 .el-menu--horizontal {
-  background: rgba(64, 64, 64, 0.7);
+  background: rgb(46, 42, 46);
 
   & > .el-menu-item.dock-right {
     color: #fff;
@@ -81,9 +83,16 @@ export default {
 }
 
 #footer {
-  padding: 20px;
   color: #fff;
   background: rgba(64, 64, 64, 0.7);
   font-size: 16px;
+}
+
+.rsg-theme {
+
+}
+
+#footer-logo {
+  height: 300px;
 }
 </style>
