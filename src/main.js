22dfc6781df59d4650cc7ea22e742828d8fd9847
roywu <gn02729362@hotmail.com>
2021-04-26 23:45:34 +0800

    增加 fontawesome package

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 
+// font-awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faCalendarAlt)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 new Vue({
   router,
   render: h => h(App)
