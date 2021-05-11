bc2daa80a39df1c8a0c748016f823c83fa435ef2
roywu <gn02729362@hotmail.com>
2021-05-12 00:39:24 +0800

    add i18n switch, add call-for-speaker and sponsor, update layout.

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -2,11 +2,15 @@
   <el-container id="main">
     <el-header id="header">
       <el-menu :default-active="activeIndex" class="el-menu rsg-theme" mode="horizontal">
-        <el-menu-item class="dock-right" index="6">票務</el-menu-item>
-        <el-menu-item class="dock-right" index="5">贊助夥伴</el-menu-item>
-        <el-menu-item class="dock-right" index="4" @click="link_to_SAI">Scrum Alliance</el-menu-item>
-        <el-menu-item class="dock-right" index="3">講者介紹</el-menu-item>
-        <el-menu-item class="dock-right" index="2">時程表</el-menu-item>
+        <el-menu-item class="dock-right" index="7">
+          <button v-if="lang=='tw'" @click="$i18n.locale='en'">English</button>
+          <button v-else @click="$i18n.locale='tw'">中文</button>
+        </el-menu-item>
+        <el-menu-item class="dock-right" index="6">{{ $t('Tickets') }}</el-menu-item>
+        <el-menu-item class="dock-right" index="5">{{ $t('Sponsors') }}</el-menu-item>
+        <el-menu-item class="dock-right" index="4" @click="redirect('https://www.scrumalliance.org/')">Scrum Alliance</el-menu-item>
+        <el-menu-item class="dock-right" index="3">{{ $t('Speakers') }}</el-menu-item>
+        <el-menu-item class="dock-right" index="2">{{ $t('Schedule') }}</el-menu-item>
         <el-menu-item class="dock-right" index="1">Scrum Gathering Taipei 2021</el-menu-item>
       </el-menu>
     </el-header>
@@ -46,7 +50,7 @@
                   <div>實踐<b>Scrum!</b> 體現<b>Scrum!</b> 讓<b>Scrum</b> 成為<b>The Future of Work。</b></div>
                   <div>（Practice and Live in Scrum!）</div>
                 </div>
-                <img class="item__image" src="@/assets/imgs/banner.jpeg"/>
+                <img class="item__image" src="@/assets/imgs/banner_210510.jpg"/>
               </div>
             </el-carousel-item>
             <el-carousel-item>
@@ -55,37 +59,75 @@
                   <h3>2021，我們一起</h3>
                   <h3>Be Agile! Run Scrum!</h3>
                 </div>
-                <img class="item__image" src="@/assets/imgs/banner.jpeg"/>
+                <img class="item__image" height="100%" src="@/assets/imgs/banner.jpeg"/>
               </div>
             </el-carousel-item>
           </el-carousel>
         </div>
       </el-row>
-
+      <el-row class="block-container">
+        <el-col :offset="5" :span="18">
+          <el-row>
+            <el-col :offset="2" :span="8">
+              <el-card :body-style="{ padding: '10px' }">
+                <img class="image"
+                     src="@/assets/imgs/callforpaper_210510.jpg">
+                <div style="padding: 14px;">
+                  <div>{{ $t('Submission') }}:
+                    <br>
+                    {{
+                      $t('The event will be held from November 4 to 6, including two-day online sessions and one-day offline activity.')
+                    }}
+                    <br>
+                    {{
+                      $t('If you would like to speak at the event, please submit your proposed session information through the link below.')
+                    }}
+                  </div>
+                  <div class="bottom clearfix">
+                    <span class="time">{{ $t('Submission closes on 30 June, 2021.') }}</span>
+                    <div style="padding-top: 15px;">
+                      <el-button class="card-btn" type="primary">{{ $t('Submission') }}</el-button>
+                    </div>
+                  </div>
+                </div>
+              </el-card>
+            </el-col>
+            <el-col :offset="2" :span="8">
+              <el-card :body-style="{ padding: '10px' }">
+                <img class="image"
+                     src="@/assets/imgs/handshake.jpeg">
+                <div style="padding: 14px;">
+                  <span>2021年全台最大敏捷盛會~等你來加入!!!
+加入RSG Taipei 2021贊助商計畫，
+幫助您的企業獲得曝光，同時招攬更多人才</span>
+                  <div class="bottom clearfix">
+                    <el-button class="card-btn" type="primary">參與贊助</el-button>
+                  </div>
+                </div>
+              </el-card>
+            </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
       <el-row class="block-container">
         <el-col :offset="3" :span="18">
-          <img id="logo-sai" src="@/assets/imgs/logo-sai.png"/>
-          <div>Founded in 2001, Scrum Alliance® is the largest, most established and influential professional member-
-            ship and certification organization in the Agile community. Scrum Alliance is a nonprofit association that
-            has certified more than 750,000 practitioners worldwide. Its vision is to help “Transform the World of Work”
-            with a mission to guide and inspire individuals, leaders, and organizations with practices, princi- ples,
-            and values that create workplaces that are joyful, prosperous, and sustainable. For more informa- tion,
-            please visit scrumalliance.org.
-          </div>
-          <div>Through Scrum Alliance, you have access to a world of learning and resources that will help you succeed
-            with Scrum in the software development industry and beyond.
-          </div>
-          <h3>Advance Yourself</h3>
-          <div>Scrum Alliance’s progressive certification course model allows you to choose an educational track that
-            improves your core Scrum knowledge, builds skill sets, and provides the necessary tools to evolve as an
-            Agile practitioner. Choose your path. Are you ready to move your career to its next sprint? Sign up for an
-            offering from our alliance of profes- sionally-assessed and specialized trainers around the globe.
-          </div>
+          <h2 style="text-align: center; font-size: 36px;">冠名贊助</h2>
+          <img id="logo-sai" class='company-logo' src="@/assets/imgs/logo-sai.png" @click="redirect('https://www.scrumalliance.org/')"/>
+        </el-col>
+      </el-row>
+      <el-row class="block-container">
+        <el-col :offset="3" :span="21">
         </el-col>
       </el-row>
       <el-row id="footer">
-        <el-col :offset="8" :span="8" style="text-align:center">
-          <img id='footer-logo' src="@/assets/imgs/RSG_logo_2021_Taipei.png"/>
+        <el-col :offset="1" :span="8">
+          Contact US: <font-awesome-icon :icon="['fas', 'envelope']" />   rsgtpe@outlook.com
+          <div>
+          <font-awesome-icon :icon="['fab', 'facebook']" class="shortcut-btn" @click="redirect('https://www.facebook.com/rsgtaipei2021')"/>
+          </div>
+        </el-col>
+        <el-col :offset="1" :span="8" style="text-align:center">
+          © 2021 Agile Community Taiwan / Taiwan Agile Tribe
         </el-col>
       </el-row>
     </el-main>
@@ -94,6 +136,7 @@
 </template>
 
 <script>
+import {i18n, locale_config} from '@/lang'
 
 export default {
   name: 'Main',
@@ -103,7 +146,7 @@ export default {
   data() {
     return {
       activeIndex: '1',
-
+      currentDate: new Date()
     }
   },
 
@@ -113,13 +156,24 @@ export default {
       let nav_height = 60
       let buffer = 240
       return (hh - nav_height - buffer) + 'px'
+    },
+    lang() {
+      return this.$i18n.locale
+    }
+  },
+
+  watch: {
+    lang() {
+      let message = locale_config[lang]
+      i18n.setLocaleMessage(lang, message)
+      this.$i18n.locale = lang
     }
   },
 
   methods: {
-    link_to_SAI() {
-      window.location.href = "https://www.scrumalliance.org/"
-    }
+    redirect(url) {
+      window.location.href = url
+    },
   },
 
   mounted() {
@@ -150,6 +204,18 @@ html, body {
 
 .item {
   font-size: 18px;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  text-align: center;
+}
+
+.item:before {
+  content: '';
+  display: inline-block;
+  height: 100%;
+  width: 0;
+  vertical-align: middle;
 }
 
 .item__content {
@@ -163,7 +229,7 @@ html, body {
   justify-content: center;
   align-items: center;
   background: #409eff;
-  opacity: 0.9;
+  opacity: 0.8;
   color: #fff;
   padding: 3px;
 
@@ -177,7 +243,8 @@ html, body {
 .item__image {
   width: 100%;
   height: auto;
-  object-fit: cover;
+  display: inline-block;
+  vertical-align: middle;
 }
 
 .el-carousel__item:nth-child(2n) {
@@ -215,6 +282,40 @@ html, body {
   }
 }
 
+.time {
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.card-btn {
+  float: right;
+  position: relative;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both
+}
+
+.company-logo {
+  cursor: pointer;
+}
+
 #logo-sai {
   display: block;
   margin-left: auto;
@@ -225,6 +326,17 @@ html, body {
   color: #fff;
   background: rgba(64, 64, 64, 0.7);
   font-size: 16px;
+  padding: 40px 30px;
+}
+
+.shortcut-btn {
+  cursor: pointer;
+  font-size: 28px;
+  margin-top: 10px;
+
+  &:hover {
+    color: #409eff;
+  }
 }
 
 .rsg-theme {
